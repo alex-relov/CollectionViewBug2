30efa5cd6ce15895c5c409b0eec61f7b0e529c28
alex <info@alex-relov.ru>
2020-11-14 13:01:06 +0500

    add sample

---
+++ CollectionViewBug2/CollectionViewBug2/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace CollectionViewBug2
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
